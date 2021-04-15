97a89ed8c3467e18b9053f42b6ad51e6cdb25cea
kammalage <shanantonfernando@gmail.com>
2021-04-14 20:56:13 -0700

    added mui pkgs and header bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
 import ShowPokedex from './components/ShowPokedex';
+import {
+    AppBar,
+    Toolbar,
+    Typography,
+    IconButton,
+    Button,
+    makeStyles,
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 import './App.css';
 
 function App() {
+    const useStyles = makeStyles((theme) => ({
+        root: {
+            flexGrow: 1,
+        },
+        menuButton: {
+            marginRight: theme.spacing(2),
+        },
+        title: {
+            flexGrow: 1,
+        },
+    }));
+
+    const classes = useStyles();
+
     return (
         <div className='App'>
             <header className='App-header'>
-                <h1>Pocket Monsters</h1>
+                <AppBar position='static'>
+                    <Toolbar>
+                        <IconButton
+                            edge='start'
+                            className={classes.menuButton}
+                            color='inherit'
+                            aria-label='menu'>
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant='h6' className={classes.title}>
+                            Pocket Monsters
+                        </Typography>
+                        <Button color='inherit'>Login</Button>
+                    </Toolbar>
+                </AppBar>
+            </header>
+            <body>
                 <h3>Create your pokemon Party</h3>
                 <ShowPokedex />
-            </header>
+            </body>
         </div>
     );
 }
